dde9720c66496e40ee2f598fca0968638bda2832
Maarten Lauwaert <github@maartenlauwaert.eu>
2025-12-10 13:57:09 +0100

    feat: add customer dashboard pages view with tree layout

---
+++ layers/customer-booking/app/pages/dashboard/[team]/pages.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BookingsPagesListVue from '~~/layers/bookings/collections/pages/app/components/List.vue'
+
+definePageMeta({
+  middleware: 'auth',
+})
+</script>
+
+<template>
+  <AppContainer title="Pages (Tree Test)">
+    <BookingsPagesListVue layout="tree" />
+  </AppContainer>
+</template>
