e2285e5310c07430888434cd01d6354c2492dac5
Maarten Lauwaert <github@maartenlauwaert.eu>
2025-12-11 14:03:05 +0100

    feat: add reserved slug validation to team creation (Task 1.3)
    - Import isReservedSlug from server/config/reserved-slugs
    - Validate team slug is not reserved before creating team
    - Return 400 error if slug is reserved

--- server/api/teams/index.post.ts
+++ server/api/teams/index.post.ts
@@ -1,10 +1,20 @@
 import { createTeam } from '@@/server/database/queries/teams'
 import { createTeamSchema } from '@@/shared/validations/team'
 import { validateBody } from '@@/server/utils/bodyValidation'
+import { isReservedSlug } from '@@/server/config/reserved-slugs'
 
 export default defineEventHandler(async (event) => {
   const { user } = await requireUserSession(event)
   const body = await validateBody(event, createTeamSchema)
+
+  // Validate slug is not reserved
+  if (isReservedSlug(body.slug)) {
+    throw createError({
+      statusCode: 400,
+      statusMessage: 'This slug is reserved and cannot be used for a team.',
+    })
+  }
+
   const team = await createTeam({
     name: body.name,
     ownerId: user.id,
