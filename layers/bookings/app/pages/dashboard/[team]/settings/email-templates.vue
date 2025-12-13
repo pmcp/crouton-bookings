5ad98ccffb18db263f7326649f2c18ef11235aba
Maarten Lauwaert <github@maartenlauwaert.eu>
2025-12-13 16:53:17 +0100

    more clean up

---
+++ layers/bookings/app/pages/dashboard/[team]/settings/email-templates.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import BookingsEmailTemplatesList from '~~/layers/bookings/collections/emailtemplates/app/components/List.vue'
+
+definePageMeta({
+  middleware: 'auth',
+})
+</script>
+
+<template>
+  <AppContainer title="Email Templates">
+    <BookingsEmailTemplatesList />
+  </AppContainer>
+</template>
