0af34e1fcfe198354c13f07233d40234aff44976
Maarten Lauwaert <github@maartenlauwaert.eu>
2025-12-14 20:51:45 +0100

    fix(bookings): include enableGroups in settings PATCH endpoint

--- layers/bookings/collections/settings/server/api/teams/[id]/bookings-settings/[settingId].patch.ts
+++ layers/bookings/collections/settings/server/api/teams/[id]/bookings-settings/[settingId].patch.ts
@@ -14,6 +14,7 @@ export default defineEventHandler(async (event) => {
 
   return await updateBookingsSetting(settingId, team.id, user.id, {
     statuses: body.statuses,
+    enableGroups: body.enableGroups,
     groups: body.groups
   })
 })
